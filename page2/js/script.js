0fda3f9959c6c96975592aecc50363ad7100b3a9
addefault <90063257+addefault@users.noreply.github.com>
2021-09-24 04:43:25 +0400

    Add files via upload

--- page2/js/script.js
+++ page2/js/script.js
@@ -3,11 +3,19 @@ $(document).ready(function() {
 		$('.steps-mobile').html($('.steps-nav p.active span').html());
 	}
 	$('.steps-nav p').click(function() {
-		$(this).addClass('active').siblings('.active').removeClass('active');
+		$(this).removeClass('medium small').addClass('active').siblings('.active').removeClass('active');
 		$('.steps-content img').eq($(this).index()).addClass('active').siblings('.active').removeClass('active');
 		$('.steps .icon').removeClass('state0 state1 state2').addClass('state'+$(this).index());
 		if($(window).width() <= 768) {
 			$('.steps-mobile').html($('.steps-nav p.active span').html());
+			if($(this).index() === 0) {
+				$(this).next().removeClass('medium small').addClass('medium').next().removeClass('medium small').addClass('small');
+			} else if($(this).index() === 1) {
+				$(this).next().removeClass('medium small').addClass('medium');
+				$(this).prev().removeClass('medium small').addClass('small');
+			} else {
+				$(this).prev().removeClass('medium small').addClass('medium').prev().removeClass('medium small').addClass('small');
+			}
 		}
 	});
 	
